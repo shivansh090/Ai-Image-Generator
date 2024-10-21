071e0ae3ecd76df1df7e8490ce97e49d7a57f700
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-10-21 12:57:17 +0530

    added about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
-import { Home, CreatePost } from './page';
+import { Home, CreatePost, About } from './page';
 
 const App = () => (
   <BrowserRouter>
@@ -10,13 +10,16 @@ const App = () => (
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
-
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      <div>
+        <Link to="/about" className="font-inter font-medium bg-gray-200 text-black px-4 py-2 rounded-md mr-5 hover:bg-gray-100" >About</Link>
+        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      </div>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path='/about' element={<About/>} />
       </Routes>
     </main>
   </BrowserRouter>
